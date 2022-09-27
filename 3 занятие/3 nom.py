5b0b6229e64ce10e6dc190c4ee17a321545104ed
MariaBarbashina <112894935+MariaBarbashina@users.noreply.github.com>
2022-09-28 02:08:53 +0300

    3 занятие

--- 3 занятие/3 nom.py
+++ 3 занятие/3 nom.py
@@ -5,12 +5,11 @@ def vr(n):
     if 0<n<=59:
         return n
     if n>59:
-        return n-60
+        return n%60
 def ch(n):
     if 0<n<=59:
         return 0
     if n>59:
-        return n//59
-#for i in range(1, 1440):
+        return n%(60*24)//60
     
-print(ch(1000), vr(1000))
+print(ch(1500), vr(1500))
