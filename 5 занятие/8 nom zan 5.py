02a93f8694ef519d37a9a68ec78242b188e729d3
MariaBarbashina <112894935+MariaBarbashina@users.noreply.github.com>
2022-10-30 21:45:23 +0300

    дз додел

--- 5 занятие/8 nom zan 5.py
+++ 5 занятие/8 nom zan 5.py
@@ -1,10 +1,15 @@
 n=int(input())
 def f(n):
     c=0
+    maxd=0
     while n!=0:
         x=int(input())
         if n==x and x!=0:
             c+=1
+        else:
+            maxd=max(maxd, c)
+            c=1
+            
         n=x
-    print(c)
+    print(maxd)
 f(n)
