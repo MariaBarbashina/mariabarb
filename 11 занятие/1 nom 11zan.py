82ca1c99fe8b507c3a318a29bd37e34eafb17738
MariaBarbashina <112894935+MariaBarbashina@users.noreply.github.com>
2022-11-28 17:36:32 +0300

    11 zan 1 nom

--- 11 занятие/1 nom 11zan.py
+++ 11 занятие/1 nom 11zan.py
@@ -1,37 +1,26 @@
 from tkinter import*
 import json
 import requests
+
+
+def saveResult(value):
+    file = open("marbar.json", "w")
+    file.writelines(json.dumps(value))
 def funct():
-    username = json.get()
+    username = jsont.get()
     url = f"https://api.github.com/users/{username}"
     user_data = requests.get(url).json()
-    with open("/Users/maria/Desktop/ВУЗ/основы программир/mariabarb/marbar.json", "w") as file:
-        if 'company' in user_data:
-            file.write(f"'company': {user_data['company']}" + '\n')
-        else:
-            file.write(f"'company': None" + '\n')
-        if 'created_at' in user_data:
-            file.write(f"'created_at':{user_data['created_at']}" + '\n')
-        else:
-            file.write(f"'created_at': None" + '\n')
-        if 'email' in user_data:
-            file.write(f"'email':{user_data['email']}" + '\n')
-        else:
-            file.write(f"'email': None")
-        if 'id' in user_data:
-            file.write(f"'id':{user_data['id']}" + '\n')
-        else:
-            file.write(f"'id': None")
-        if 'name' in user_data:
-            file.write(f"'name':{user_data['name']}" + '\n')
-        else:
-            file.write(f"'name': None")
-        if 'url' in user_data:
-            file.write(f"'url':{user_data['url']}" + '\n')
-        else:
-            file.write(f"'url': None")
-    lbl.configure(text = 'Нужные данные заипсаны в файле')
 
+    keys=['company', 'created_at', 'email', 'id', 'name', 'url']
+    result : dict = {}
+
+    for i in range(len(keys)):
+        result[keys[i]] = user_data[keys[i]]
+
+    saveResult(result)
+
+
+    lbl.configure(text = 'Нужные данные заипсаны в файле')
 
 root = Tk()
 root.title('Барбашина Мария Назаровна У-222(1)')
@@ -40,6 +29,6 @@ lbl = Label(root,text = 'Напишите имя пользователя github
 lbl.grid(column=0,row=0)
 btn = Button(root,text = 'открыть',command = funct)
 btn.grid(column=2,row=0)
-txt = Entry(root,width=10)
-txt.grid(column=1,row=0)
+jsont = Entry(root,width=10)
+jsont.grid(column=1,row=0)
 root.mainloop()
